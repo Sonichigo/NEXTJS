4c9906cd46824176c02544e7a505f5cda6ab2dcf
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2022-02-21 20:00:17 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,19 +1,16 @@
 import Head from 'next/head'
 import React from "react";
-import { useRouter } from 'next/router'
 import SawoLogin from "sawo-react"
 import lstyles from '../styles/login.module.css'
 function login() {
-  const router = useRouter()
     function sawoLoginCallback(payload) {
         console.log("Payload : " + JSON.stringify(payload));
-        router.push('/')
     }
 
     const sawoConfig = {
         onSuccess: sawoLoginCallback,
         identifierType: "email", //required, must be one of: 'email', 'phone_number_sms',
-        apiKey: "d7542d08-230b-48b0-a465-2a2a444cce4f", // required, get it from sawo dev.sawolabs.com,
+        apiKey: process.env.API_KEY, // required, get it from sawo dev.sawolabs.com,
         containerHeight: "300px", // the login container height, default is 300px
     };
 
